4a19d4bd1fb88c3f9d4e9237166005f2ea9c1977
benisi <bisidahomen@gmail.com>
2021-03-25 09:13:06 +0100

    add registration logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends BaseController
 {
@@ -23,6 +27,20 @@ class AuthController extends BaseController
             } else {
                 return response(__('invalid credentials'), Response::HTTP_UNAUTHORIZED);
             }
+        } catch (\Exception $e) {
+            return response(__('something went wrong'), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $data = $request->only(['name', 'email', 'password']);
+            $data['password'] = Hash::make($data['password']);
+            $data['email'] = trim($data['email']);
+            $user = User::create($data);
+            $token = JWTAuth::fromUser($user);
+            return response(['message' => __('user was created successfully'), 'token' => $token, 'data' => $user], Response::HTTP_CREATED);
         } catch (\Exception $e) {
             return response(__('something went wrong'), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -12,6 +12,7 @@ class UserControllerTest extends TestCase
     use RefreshDatabase;
 
     const LOGIN_URL = 'api/login';
+    const REGISTER_URL = 'api/register';
 
     public function test_can_login_user()
     {
@@ -53,17 +54,111 @@ class UserControllerTest extends TestCase
                 ]
             ]);
 
-            $request = [
-                'email' => 'lets test',
-                'password' => 'password'
-            ];
+        $request = [
+            'email' => 'lets test',
+            'password' => 'password'
+        ];
 
-            $response = $this->postJson(self::LOGIN_URL, $request);
+        $response = $this->postJson(self::LOGIN_URL, $request);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'email' => ["The email must be a valid email address."],
+                ]
+            ]);
+    }
+
+    public function test_add_new_user()
+    {
+        $request = [
+            'name' => 'Benjamin Isidahomen',
+            'email' => 'bisidahomen@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $this->postJson(self::REGISTER_URL, $request)
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJson([
+                'data' => [
+                    'name' => 'Benjamin Isidahomen',
+                    'email' => 'bisidahomen@gmail.com'
+                ]
+            ]);
+    }
+
+    public function test_failed_to_add_user_when_input_is_invalid()
+    {
+        $request = [
+            'email' => 'bisidahomen@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->postJson(self::REGISTER_URL, $request);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'name' => ['The name field is required.'],
+                ]
+            ]);
+
+        $request = [
+            'name' => 'benjamin',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->postJson(self::REGISTER_URL, $request);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'email' => ['The email field is required.'],
+                ]
+            ]);
+
+        $request = [
+            'name' => 'benjamin',
+            'email' => 'bisidahomen@gmail.com',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->postJson(self::REGISTER_URL, $request);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'password' => ['The password field is required.'],
+                ]
+            ]);
+
+        $request = [
+            'name' => 'benjamin',
+            'email' => 'bisidahomen@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'passwor'
+        ];
+
+        $response = $this->postJson(self::REGISTER_URL, $request);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'password' => ['The password confirmation does not match.'],
+                ]
+            ]);
+
+            $request = [
+                'name' => 'benjamin',
+                'email' => 123,
+                'password' => 'password',
+                'password_confirmation' => 'password'
+            ];
     
+            $response = $this->postJson(self::REGISTER_URL, $request);
             $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
                 ->assertJson([
                     'errors' => [
-                        'email' => ["The email must be a valid email address."],
+                        'email' => ['The email must be a valid email address.'],
                     ]
                 ]);
     }
